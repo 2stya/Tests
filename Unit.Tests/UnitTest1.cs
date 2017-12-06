93b4c31cc3444017b52812a089b3ee03b8f7e5c1
Konstantin Kravchenko <konstantin.kravchenko@dfsco.com>
2017-12-06 13:43:47 +0200

    Initial tests commit

---
+++ Unit.Tests/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using Unit;
+using Xunit;
+
+namespace Unit.Tests
+{
+    public class UnitTest1
+    {
+        private static FizzBuzz fizzBuzz;
+        public UnitTest1()
+        {
+            fizzBuzz = new FizzBuzz();
+        }
+
+        [Fact]
+        public void ReturnBuzzFizzIfInputDividesBy3And5()
+        {
+            string expected = "FizzBuzz";
+            string output = fizzBuzz.GetOutput(15);
+
+            Assert.Equal(expected, output);
+        }
+
+        [Fact]
+        public void ReturnBuzzFizzIfInputNotDividesBy3Nor5()
+        {
+            string expected = "2";
+            string output = fizzBuzz.GetOutput(2);
+
+            Assert.Equal(expected, output);
+        }
+
+        [Fact]
+        public void ReturnBuzzFizzIfInputDividesBy3ButNot5()
+        {
+            string expected = "Fizz";
+            string output = fizzBuzz.GetOutput(9);
+
+            Assert.Equal(expected, output);
+        }
+
+        [Fact]
+        public void ReturnBuzzFizzIfInputDividesBy5ButNot3()
+        {
+            string expected = "Buzz";
+            string output = fizzBuzz.GetOutput(10);
+
+            Assert.Equal(expected, output);
+        }
+
+    }
+}
